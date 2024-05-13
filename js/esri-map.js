402d0768c63f680d7bbc2acfdae65e63b927efdc
Choleski Louis <louischoleski@gmail.com>
2024-05-13 02:37:31 -0400

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -23,7 +23,7 @@ WildRydes.map = WildRydes.map || {};
         var map = new Map({ basemap: 'gray-vector' });
 
         var view = new MapView({
-            center: [45.5337339, -73.6803549],
+            center: [-73.6803549, 45.5337339],
             container: 'map',
             map: map,
             zoom: 12
